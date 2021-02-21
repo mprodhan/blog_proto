9a4852cb8654986670edea2c596a17029c5cfb43
Mustafa Prodhan <mustafa.prodhan.317@gmail.com>
2021-02-21 14:50:36 -0500

    setup model, abstract user and the AUTH_USER_MODEL.

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Author(AbstractUser):
+    name = models.CharrField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+class Blog(models.Model):
+    title = models.CharField(max_length=30)
+    body = models.TextField()
+    date = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
