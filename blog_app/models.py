a099c6a594e552d877c7e3ac0212833bbe10ca70
Mustafa Prodhan <mustafa.prodhan.317@gmail.com>
2021-03-19 22:11:27 -0400

    successfully uploaded image to blog.

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,3 +1,4 @@
+import os
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import AbstractUser
@@ -19,5 +20,11 @@ class Blog(models.Model):
     def __str__(self):
         return self.title
 
-
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        upload_img = Image.open(self.image.path)
+        if upload_img.height > 300 or upload_img > 300:
+            output_size = (300,300)
+            upload_img.thumbnail(output_size)
+            upload_img.save(self.image.path)
 
